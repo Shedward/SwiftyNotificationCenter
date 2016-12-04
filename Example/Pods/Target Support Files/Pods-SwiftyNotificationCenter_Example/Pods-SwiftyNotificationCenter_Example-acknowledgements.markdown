b803d60e1aaa0776eb1c811f03dac4df299e0d0e
Vladislav Maltsev <malcev@azoft.com>
2016-12-04 20:17:29 +0700

    Add some tests.

--- Example/Pods/Target Support Files/Pods-SwiftyNotificationCenter_Example/Pods-SwiftyNotificationCenter_Example-acknowledgements.markdown
+++ Example/Pods/Target Support Files/Pods-SwiftyNotificationCenter_Example/Pods-SwiftyNotificationCenter_Example-acknowledgements.markdown
@@ -3,7 +3,7 @@ This application makes use of the following third party libraries:
 
 ## SwiftyNotificationCenter
 
-Copyright (c) 2016 Vladislav Maltsev <malcev@azoft.com>
+Copyright (c) 2016 Vladislav Maltsev <shedwardx@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,9 +2,59 @@
 
 import Quick
 import Nimble
+
 import SwiftyNotificationCenter
 
-class TableOfContentsSpec: QuickSpec {
+struct TestEvent: PostableEvent, ObservableEvent {
+    let testValue: String
+    
+    init(value:String) {
+        self.testValue = value
+    }
+}
+
+class TestObject {
+    
+}
+
+let testObject = TestObject()
+
+class SwiftyNotificationCenterSpec: QuickSpec {
     override func spec() {
+        describe("NotificationCenter.post(event:)") {
+            it("post only one event") {
+                expect {
+                    NotificationCenter.default.post(event: TestEvent(value: "test"))
+                }.to(postNotifications(haveCount(1)))
+            }
+            
+            it("post event with proper name") {
+                expect {
+                    NotificationCenter.default.post(event: TestEvent(value: "test"))
+                }.to(postNotifications(allPass({ notification in
+                    guard let notification = notification else { return false }
+                    return notification.name.rawValue == TestEvent.notificationName().rawValue
+                })))
+            }
+            
+            it("post event from proper object") {
+                expect {
+                    NotificationCenter.default.post(event: TestEvent(value: "test"), fromObject: testObject)
+                }.to(postNotifications(allPass({ notification in
+                    guard let object = notification?.object as? TestObject else { return false }
+                    return object === testObject
+                })))
+            }
+            
+            it("send event data in userInfo") {
+                let expectedEvent = TestEvent(value: "test")
+                expect {
+                    NotificationCenter.default.post(event: expectedEvent)
+                }.to(postNotifications(allPass({ notification in
+                    guard let event = notification?.userInfo?["SwiftyNotificationData"] as? TestEvent else { return false }
+                    return event.testValue == expectedEvent.testValue
+                })))
+            }
+        }
     }
 }
